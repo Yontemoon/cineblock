cd501582bb42a3182cbed1b303de4c88aff2398c
YonteMoon <monteyoon1998@gmail.com>
2024-10-06 22:57:34 -0700

    Added basic footer

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,12 +1,14 @@
 import * as React from "react";
 import { Outlet, createRootRoute } from "@tanstack/react-router";
 import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 export const Route = createRootRoute({
   component: () => (
     <React.Fragment>
       <Navbar />
       <Outlet />
+      <Footer />
     </React.Fragment>
   ),
 });
